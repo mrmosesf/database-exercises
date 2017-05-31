7e9db66b0bd59e6cea70028541a9d4057ca6fa61
Moses Franco <mosesf@outlook.com>
2017-05-31 11:02:03 -0500

    Completed tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+-- Albums exercise
+
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  artist VARCHAR(255),
+  name VARCHAR(255),
+  release_date YEAR,
+  sales FLOAT,
+  genre VARCHAR(100)
+)
