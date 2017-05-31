f2e9c0da9c6beafdbc60bb41402b4d36060cb25f
Moses Franco <mosesf@outlook.com>
2017-05-31 12:26:07 -0500

    A seed to fil a database with albums that have sold 30 million or more copies

---
+++ basic_statement/albums_seeder.sql
@@ -0,0 +1,41 @@
+-- Albums exercise
+-- Demo insert
+
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982', '65', 'pop, rock, r&b');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('AC/DC', 'Back in Black', '1980', '50', 'hard rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Pink Floyd', 'The Dark Side of the Moon', '1973', '44', 'progressive rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Whitney Houston/Various artists', 'The Bodyguard', '1992', '44', 'r&b, soul, pop, soundtrack'),
+  ('Meat Loaf', 'Bat Out of Hell', '1977', '43', 'hard rock, progressive rock'),
+  ('Eagles', 'Their Greatest Hits(1971-1975)', '1976', '42', 'rock, soft rock, folk rock'),
+  ('Bee Gees/Various Artists', 'Saturday Night Fever', '1977', '40', 'disco'),
+  ('Fleetwood Mac', 'Rumours', '1977', '40', 'soft rock'),
+  ('Shania Twain', 'Come On Over', '1997', '39', 'country, pop'),
+  ('Led Zeppelin', 'Led Zeppelin IV', '1971', '37', 'hard rock, heavy metal, folk'),
+  ('Michael Jackson', 'Bad', '1987', '34', 'pop, funk rock'),
+  ('Alanis Morissette', 'Jagged Little Pill', '1995', '34', 'alternative rock'),
+  ('Celine Dion', 'Falling Into You', '1996', '33', 'pop, soft rock'),
+  ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', '1967', '32', 'rock'),
+  ('Eagles', 'Hotel California', '1976', '32', 'rock, soft rock, folk rock'),
+  ('Mariah Carey', 'Music Box', '1993', '32', 'pop, r&b, rock'),
+  ('Michael Jackson', 'Dangerous', '1991', '32', 'rock, pop, funk'),
+  ('Various artists', 'Dirt Dancing', '1987', '32', 'pop, rock, r&b'),
+  ('Adele', '21', '2011', '31', 'pop, soul'),
+  ('Celine Dion', 'Let''s Talk About Love', '1997', '31', 'pop, soft rock'),
+  ('The Beatles', '1', '2000', '31', 'rock'),
+  ('The Beatles', 'Abbey Road', '1969', '30', 'rock'),
+  ('Bruce Springsteen', 'Born in the U.S.A', '1984', '30', 'rock'),
+  ('Dire Straits', 'Brothers in Arms', '1985', '30', 'rock'),
+  ('James Horner', 'Titanic: Music from the Motion Picture', '1997', '30', 'soundtrack'),
+  ('Madonna', 'The Immaculate Collection', '1990', '30', 'pop, dance'),
+  ('Metallica', 'Metallica', '1991', '30', 'thrash metal, heavy metal'),
+  ('Nirvana', 'Nevermind', '1991', '30', 'progressive rock'),
+  ('Pink Floyd', 'The Wall', '1979', '30', 'progressive rock'),
+  ('Santana', 'Supernatural', '1999', '30', 'rock'),
+  ('Guns N'' Roses', 'Appetite for Destruction', '1987', '30', 'heavy metal, hard rock'),
+  ('Whitney Houston', 'Whitney Houston', '1985', '30', 'pop, r&b');
