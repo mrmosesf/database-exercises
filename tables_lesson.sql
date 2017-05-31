71533fa98979853b85ad42918e55701884e3c139
Moses Franco <mosesf@outlook.com>
2017-05-31 11:02:23 -0500

    Follow along with instructor

--- tables_lesson.sql
+++ tables_lesson.sql
@@ -1,1 +1,10 @@
 -- Tables lesson
+
+USE codeup_test_db;
+
+CREATE TABLE airport(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  airport_code CHAR(3) NOT NULL,
+  location VARCHAR(255),
+  description TEXT
+)
