74b67f57793f01f69db384a6ad4fe155abfb9b6e
Moses Franco <mosesf@outlook.com>
2017-06-01 10:21:41 -0500

    Rewrote select to delete, and after server seeds, can confirm, it works; no more music after 1991, no more disco, and no more Whitney Houston

--- sql/delete_exercise.sql
+++ sql/delete_exercise.sql
@@ -4,22 +4,22 @@
 USE codeup_test_db;
 
 SELECT 'Albums released after 1991' AS '';
-SELECT
-  name,
-  release_date
+DELETE
+#   name,
+#   release_date
 FROM albums
 WHERE release_date > 1991;
 
 SELECT 'Albums with the genre ''disco''' AS '';
-SELECT
-  name,
-  genre
+DELETE
+#   name,
+#   genre
 FROM albums
 WHERE genre LIKE '%disco%';
 
 SELECT 'Albums by ''Whitney Houston''' AS '';
-SELECT
-  name,
-  artist
+DELETE
+#   name,
+#   artist
 FROM albums
 WHERE artist LIKE '%Whitney Houston%';
