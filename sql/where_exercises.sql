a75a7e9cabe7d2d8c3d2d2aa47c678a2a7f942f5
Moses Franco <mosesf@outlook.com>
2017-06-01 15:48:18 -0500

    Updated to complete the Where continued exercise

--- sql/where_exercises.sql
+++ sql/where_exercises.sql
@@ -3,12 +3,18 @@ USE employees;
 # Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
 SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+      AND gender = 'M';
 
 # Employees whose last name starts with 'E' — 7,330 rows.
 SELECT *
 FROM employees
 WHERE last_name LIKE ('E%');
+
+# All employees whose last name starts and ends with 'E'
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
 # Employees hired in the 90s — 135,214 rows.
 SELECT *
@@ -21,7 +27,17 @@ SELECT *
 FROM employees
 WHERE birth_date LIKE '%-12-25';
 
+# Employees born on Christmas and hired in the 90s - 362 rows
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25' AND hire_date BETWEEN date_format('1990-01-01', '%Y-%m-01') AND LAST_DAY('1999-12-01');
+
 # Employees with a 'q' in their last name — 1,873 rows.
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
+
+# All employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
