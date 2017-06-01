37c3f9583f6c3e212e8d6df74b4a18c46dfe7120
Moses Franco <mosesf@outlook.com>
2017-06-01 16:06:06 -0500

    Initial commit, copying from previous exercise

---
+++ sql/order_by_exercises.sql
@@ -0,0 +1,42 @@
+USE employees;
+
+# Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+      AND gender = 'M';
+
+# Employees whose last name starts with 'E' — 7,330 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+# All employees whose last name starts and ends with 'E'
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+# Employees hired in the 90s — 135,214 rows.
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN date_format('1990-01-01', '%Y-%m-01') AND LAST_DAY('1999-12-01');
+
+# Employees born on Christmas — 842 rows.
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+# Employees born on Christmas and hired in the 90s - 362 rows
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25' AND hire_date BETWEEN date_format('1990-01-01', '%Y-%m-01') AND LAST_DAY('1999-12-01');
+
+# Employees with a 'q' in their last name — 1,873 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+# All employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
