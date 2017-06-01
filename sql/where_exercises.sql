415a8116335ad1ee8ae26a1034cd5df49315a0e1
Moses Franco <mosesf@outlook.com>
2017-06-01 13:40:08 -0500

    Completed exercise, tested using the database console

---
+++ sql/where_exercises.sql
@@ -0,0 +1,26 @@
+USE employees;
+
+# Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+# Employees whose last name starts with 'E' — 7,330 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%');
+
+# Employees hired in the 90s — 135,214 rows.
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+# Employees born on Christmas — 842 rows.
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+# Employees with a 'q' in their last name — 1,873 rows.
+SELECT DISTINCT *
+FROM employees
+WHERE last_name LIKE '%q%';
