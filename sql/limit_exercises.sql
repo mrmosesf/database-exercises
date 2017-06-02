86dab42d8fbd3f7837954a14a2d6c13c6723e274
Moses Franco <mosesf@outlook.com>
2017-06-02 10:40:01 -0500

    Employees born on Christmas and hired in the 90s limited to 5

--- sql/limit_exercises.sql
+++ sql/limit_exercises.sql
@@ -4,3 +4,10 @@ SELECT last_name
 FROM employees
 GROUP BY last_name DESC
 LIMIT 10;
+
+# Employees born on Christmas and hired in the 90s limited to 5
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25' AND hire_date BETWEEN '1990-01-01' AND LAST_DAY('1999-12-01')
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
