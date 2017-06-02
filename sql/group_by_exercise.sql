2b4d83a9aaf0c317c5247b86808112b6ec5ced35
Moses Franco <mosesf@outlook.com>
2017-06-02 09:25:56 -0500

    All employees whose last name starts and ends with 'E' grouped by last name

--- sql/group_by_exercise.sql
+++ sql/group_by_exercise.sql
@@ -2,3 +2,9 @@ USE employees;
 
 SELECT DISTINCT titles.title
 FROM titles ORDER BY title;
+
+# All employees whose last name starts and ends with 'E' grouped by last name
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%E'
+GROUP BY last_name;
